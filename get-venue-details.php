81391cf73bf436b2d58f1c75e285e2f6fe0ff8e6
[Dawid Wraga] <[dpwraga@gmail.com]>
2022-04-09 13:07:54 +0100

    added php connection to non-uni database

--- get-venue-details.php
+++ get-venue-details.php
@@ -1,7 +1,8 @@
 <?php
 
-$conn = mysqli_connect("sci-mysql","coa123wuser","grt64dkh!@2FD","coa123wdb");
-if(!$conn) echo "Warning: connection error: ". mysqli_connect_error();
+$conn = mysqli_connect("spryrr1myu6oalwl.chr7pe7iynqr.eu-west-1.rds.amazonaws.com","y21blfp42bybvehd","klclvqvgzdqq368i","ifjq14krys29jh90");
+// $conn = mysqli_connect("sci-mysql","coa123wuser","grt64dkh!@2FD","coa123wdb");
+if(!$conn) die("Connection failed: ". mysqli_connect_error());
 
 function validate($data) {
   $data = htmlspecialchars($data);
